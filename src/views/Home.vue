b01b76098f09a78a936e28c9b9d7a87c83e272fc
aixinaihongjun <1181391196@qq.com>
2021-10-21 17:17:57 +0800

    创建vue实例及album界面

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>
+      <router-link to="/about">
+        <button type="button" class="btn btn-primary mr-1">About</button>
+      </router-link>
+      <router-link to="/album">
+        <button type="button" class="btn btn-primary ml-1">Album</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
